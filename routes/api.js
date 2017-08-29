62a632170c5285da61960fcce086f483766b4b61
mxdi9i7 <peter.zheng88228@gmail.com>
2017-08-29 15:09:02 -0400

    api

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+/* GET home page. */
+
+router.post('/',(req,res,next)=>{
+    var { username } = req.query
+    axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${username}?api_key=RGAPI-50633443-46a4-4f96-8156-ad91df56a9b3`)
+    .then((name)=> {
+        res.send(name.data)
+    })
+})
+
+// router.get('/', function(req, res, next) {
+//   axios.get('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/localhost3000?api_key=RGAPI-50633443-46a4-4f96-8156-ad91df56a9b3')
+//   .then((name)=> {
+//       console.log(name.data)
+//   })
+// });
+
+module.exports = router;
